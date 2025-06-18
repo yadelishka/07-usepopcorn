d6dcb68a6afe99ee09bd49b9ac003ded25f86688
Iashkina Adelina <yadelishka@gmail.com>
2025-06-18 14:22:41 +0300

    add props as API to StarRating

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+//import "./index.css";
+//import App from "./App";
+import StarRating from "./StarRaiting";
+import { useState } from "react";
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating color="blue" maxRaiting={10} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating size={24} color="red" className="test" defaultRating={3} />
+
+    <Test />
   </React.StrictMode>
 );
