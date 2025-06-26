0e3c0b2b48d34198f278b77d1986163dc04f6a99
Iashkina Adelina <yadelishka@gmail.com>
2025-06-26 18:28:28 +0300

    add clean up func for title

--- src/App.js
+++ src/App.js
@@ -336,6 +336,19 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
     [selectedId]
   );
 
+  useEffect(
+    function () {
+      if (!title) return;
+      document.title = `Movie | ${title}`;
+
+      return function () {
+        document.title = "usePopcorn";
+        console.log(`Clean up effect for movie ${title}`);
+      };
+    },
+    [title]
+  );
+
   return (
     <div className="details">
       {isLoading ? (
