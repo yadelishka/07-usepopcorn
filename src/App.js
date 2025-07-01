7ae9f8df8a8a9a15085342c3dd55b3889ac1fc3b
Adelina <yadelishka@gmail.com>
2025-07-01 21:29:59 +0400

    fix null in watched state problem

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ export default function App() {
   // const [watched, setWatched] = useState([]);
   const [watched, setWatched] = useState(function () {
     const storedValue = localStorage.getItem("watched");
-    return JSON.parse(storedValue);
+    return storedValue ? JSON.parse(storedValue) : [];
   });
 
   function handleSelectMovie(id) {
